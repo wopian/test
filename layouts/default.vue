1bad708d83571ed2a0a05088e180a43e8909b695
James Harris <wopian@wopian.me>
2018-09-07 22:48:28 +0100

    feat: restore footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
   div
-    appHeader
+    app-header
     nuxt
+    app-footer
 </template>
 
 <script>
+import AppFooter from '~/components/AppFooter'
 import AppHeader from '~/components/AppHeader'
 
 export default {
   components: {
+    AppFooter,
     AppHeader
   }
 }
