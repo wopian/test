ec6770d2f6ce177d66d41325a744cfa696b90674
James Harris <wopian@wopian.me>
2018-09-09 02:09:50 +0100

    feat: populate post and about page description (#19)

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,16 @@
 <template lang="pug">
   p Placeholder
 </template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: `About - ${process.env.title}`,
+        meta: [
+          { hid: 'description', name: 'description', content: `About ${process.env.title}` }
+        ]
+      }
+    }
+  }
+</script>
